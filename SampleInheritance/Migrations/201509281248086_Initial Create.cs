36106951c403b2b54e69d3d1885535aeb98cf399
Dheeraj Kumar Gunti <guntidheerajkumar@gmail.com>
2016-07-06 16:09:29 +0530

    Inheritance in Entity Framework
    This step-by-step walkthrough shows how to implement table-per-hierarchy
    (TPH) inheritance in your conceptual model with the Entity Framework
    Designer (EF Designer). TPH inheritance uses one database table to
    maintain data for all of the entity types in an inheritance hierarchy.

---
+++ SampleInheritance/Migrations/201509281248086_Initial Create.cs
@@ -0,0 +1,30 @@
+namespace WebApplication5.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.People",
+                c => new
+                    {
+                        PersonId = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        HireDate = c.DateTime(),
+                        EnrollmentDate = c.DateTime(),
+                        Discriminator = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => t.PersonId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.People");
+        }
+    }
+}
---
+++ SampleInheritance/Models/SchoolContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+        public DbSet<Instructor> Insructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        public SchoolContext()
+            : base("DefaultConnection")
+        {
+
+        }
+    }
+}
